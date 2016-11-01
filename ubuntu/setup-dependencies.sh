fc40308cef52a1da3cb6b5aa562d97d29cafd4dc
thisisaaronland <whosonfirst@mapzen.com>
2016-11-01 16:40:13 -0700

    what would circle do (per issue #24)

--- ubuntu/setup-dependencies.sh
+++ ubuntu/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install -y make nginx gunicorn python-gevent python-flask
+sudo apt-get install -y make python-pip nginx gunicorn python-gevent python-flask
 
-sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
+sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
