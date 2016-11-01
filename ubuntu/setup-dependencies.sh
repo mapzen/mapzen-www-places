6e139f97dd83560763bb80915e9f807a9b19493b
thisisaaronland <whosonfirst@mapzen.com>
2016-11-01 11:35:11 -0700

    ensure that py-mz-wof-placetypes is installed per issue #24

--- ubuntu/setup-dependencies.sh
+++ ubuntu/setup-dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 sudo apt-get install -y make nginx gunicorn python-gevent python-flask
+
+sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
